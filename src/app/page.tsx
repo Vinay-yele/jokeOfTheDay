b242f87dc505d083ca372ed9c01d90fdfa15d163
Vinay Yele <vinayyele@Vinays-MacBook-Air.local>
2024-12-22 19:59:50 +0530

    sl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import QuoteOfTheDay from "../components/QuoteOfTheDay"
-
+import Imag from "../../public/image.png"
+import Image from "next/image"
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-r from-cyan-500 to-blue-500">
-      <h1 className="text-4xl font-bold text-white mb-8">Joke of the Day</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24  from-cyan-500 to-blue-500">
+     <div className="absolute inset-0 -z-10">
+        <Image 
+          src={Imag} 
+          alt="Background Image" 
+          layout="fill" 
+          objectFit="cover" 
+          priority 
+        />
+      </div>      <h1 className="text-4xl font-bold text-black mb-8">Joke of the Day</h1>
       <QuoteOfTheDay />
     </main>
   )
